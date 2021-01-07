14d374586e1239dc566dfe8976e22721797771a3
Luís Silvério <oppositedeep@gmail.com>
2021-01-07 21:29:41 +0000

    Corrigi a utilização do ficheiro do perceptron

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -3,12 +3,13 @@ import pandas as pd
 import nltk
 from nltk.corpus import words
 
-## Globais
+# Globais
 try:
     nltk.data.find("words")
 except LookupError:
     nltk.download("words")
 dictionary = set(words.words())
+
 
 def train(ficheiro):
     file = pd.read_csv(f"{ficheiro}.csv", encoding="latin-1")
@@ -17,28 +18,34 @@ def train(ficheiro):
         message = file.iloc[i, 1].split()  # mensagem separada
         for word in message:
             if word.lower() not in bagOfWords and word.lower() in dictionary:
-                bagOfWords[word] = len(bagOfWords)  # atribui uma posicao a palavra
+                # atribui uma posicao a palavra
+                bagOfWords[word] = len(bagOfWords)
     generate(str(ficheiro))
 
 
 def generate(ficheiro):
-    file = pd.read_csv(f"{ficheiro}.csv", encoding="latin-1")  # le o ficheiro novamente porque é suposto usarmos dois ficheiros
+    # le o ficheiro novamente porque é suposto usarmos dois ficheiros
+    file = pd.read_csv(f"{ficheiro}.csv", encoding="latin-1")
     fileLines = file.shape[0]
 
     global data
-    data = np.zeros((file.shape[0], len(bagOfWords)))  # matriz que vê as vezes que as palavras foram repetidas
+    # matriz que vê as vezes que as palavras foram repetidas
+    data = np.zeros((file.shape[0], len(bagOfWords)))
     global verify
-    verify = np.zeros(file.shape[0])  # matriz que vê as palavras que sao spam ou ham
+    # matriz que vê as palavras que sao spam ou ham
+    verify = np.zeros(file.shape[0])
 
     for i in range(fileLines):
         message = file.iloc[i, 1].split()
         for word in message:
             if word.lower() in bagOfWords:  # ve se a palavra está no bagofwords que o algoritmo conhece
-                data[i, bagOfWords[word.lower()]] += 1  # o algoritmo reconheceu a palavra e coloca-a na matriz para vermos quantas vezes vai repetir-se
+                # o algoritmo reconheceu a palavra e coloca-a na matriz para vermos quantas vezes vai repetir-se
+                data[i, bagOfWords[word.lower()]] += 1
                 if file.iloc[i, 0] == 'spam':
                     verify[i] = 1  # é spam portanto 1
                 elif file.iloc[i, 0] == 'ham':
                     verify[i] = -1  # é ham portanto -1
+
 
 class Perceptron():
     def __init__(self, l_rate, n_iter):
@@ -68,20 +75,24 @@ class Perceptron():
 
 if __name__ == "__main__":
     bagOfWords = {}
-    train("Treino")
+    train("spam")
     weight = np.zeros(1 + data.shape[1])
-    P = Perceptron(0.01, 20)
+    P = Perceptron(0, 20)
     P.classify(data, verify)
-    generate("Treino")
+    generate("spam")
     P = Perceptron(0, 1)
     classification = P.classify(data, verify)
 
-    print("Número de linhas analisadas: {}".format(data.shape[0]))
-    print("Número de palavras existentes (sem repetição): {}".format(data.shape[1]))
+    print("\nNúmero de linhas analisadas: {}".format(data.shape[0]))
+    print("Número de palavras existentes (sem repetição): {}".format(
+        data.shape[1]))
     print("Número de mensagens classificadas como spam: {}".format(sum(verify == 1)))
     print("Número de mensagens classificadas como ham: {}".format(sum(verify == -1)))
 
-    print("O que o algoritmo percebeu:")
-    print("Número de mensagens classificadas como spam: {}".format(sum(classification == 1)))
-    print("Número de mensagens classificadas como ham: {}".format(sum(classification == -1)))
-    print("A precisão do algoritmo é de: {:0.3f} %".format(sum(classification == verify) / data.shape[0] * 100))
+    print("\nO que o algoritmo percebeu:")
+    print("Número de mensagens classificadas como spam: {}".format(
+        sum(classification == 1)))
+    print("Número de mensagens classificadas como ham: {}".format(
+        sum(classification == -1)))
+    print("A precisão do algoritmo é de: {:0.3f} %".format(
+        sum(classification == verify) / data.shape[0] * 100))
